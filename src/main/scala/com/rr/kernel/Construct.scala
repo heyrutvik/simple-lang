889c3774e01c15f8977dc72fe6433e12710564e4
Rutvik <heyrutvik@gmail.com>
2018-04-01 15:10:18 +0530

    impl not yet decided

--- src/main/scala/com/rr/kernel/Construct.scala
+++ src/main/scala/com/rr/kernel/Construct.scala
@@ -1,11 +1,10 @@
 package com.rr.kernel
 
 trait Construct[A] {
-  type S <: Syntax
   def string(a: A): String = a.toString
   def syntax(a: A): String
   def isReducible(a: A): Boolean
-  def reduce(a: A): S
+  def reduce(a: A): Syntax
 }
 
 object Construct {
@@ -19,37 +18,34 @@ object ConstructSyntax {
     def string(implicit c: Construct[A]): String = c.string(value)
     def syntax(implicit c: Construct[A]): String = c.syntax(value)
     def isReducible(implicit c: Construct[A]): Boolean = c.isReducible(value)
-    def reduce(implicit c: Construct[A]): c.S = c.reduce(value)
+    def reduce(implicit c: Construct[A]): Syntax = c.reduce(value)
   }
 }
 
 object ConstructImplicits {
 
   implicit val numberConstruct = new Construct[Num] {
-    override type S = Num
     override def syntax(a: Num): String = s"${a.value.toString}"
     override def isReducible(a: Num): Boolean = false
-    override def reduce(a: Num): S = a
+    override def reduce(a: Num): Num = a
   }
 
-  implicit def addConstruct[A, B](implicit lc: Construct[A], rc: Construct[B]) = new Construct[Add[A, B]] {
-    override type S <: Syntax
-    override def syntax(a: Add[A, B]): String = s"${lc.syntax(a.left)} + ${rc.syntax(a.right)}"
-    override def isReducible(a: Add[A, B]): Boolean = true
-    override def reduce(a: Add[A, B]): S = a match {
-      case Add(left, right) if lc.isReducible(left) => Add(lc.reduce(left), right)
-      case Add(left, right) if rc.isReducible(right) => Add(left, rc.reduce(right))
+  implicit def addConstruct(implicit cs: Construct[Syntax]) = new Construct[Add] {
+    override def syntax(a: Add): String = s"${cs.syntax(a.left)} + ${cs.syntax(a.right)}"
+    override def isReducible(a: Add): Boolean = true
+    override def reduce(a: Add): Syntax = a match {
+      case Add(left, right) if cs.isReducible(left) => Add(cs.reduce(left), right)
+      case Add(left, right) if cs.isReducible(right) => Add(left, cs.reduce(right))
       case Add(Num(x), Num(y)) => Num(x + y)
     }
   }
 
-  implicit def mulConstruct[A, B](implicit lc: Construct[A], rc: Construct[B]) = new Construct[Mul[A, B]] {
-    override type S <: Syntax
-    override def syntax(a: Mul[A, B]): String = s"${lc.syntax(a.left)} * ${rc.syntax(a.right)}"
-    override def isReducible(a: Mul[A, B]): Boolean = true
-    override def reduce(a: Mul[A, B]): S = a match {
-      case Mul(left, right) if lc.isReducible(left) => Mul(lc.reduce(left), right)
-      case Mul(left, right) if rc.isReducible(right) => Mul(left, rc.reduce(right))
+  implicit def mulConstruct(implicit cs: Construct[Syntax]) = new Construct[Mul] {
+    override def syntax(a: Mul): String = s"${cs.syntax(a.left)} * ${cs.syntax(a.right)}"
+    override def isReducible(a: Mul): Boolean = true
+    override def reduce(a: Mul): Syntax = a match {
+      case Mul(left, right) if cs.isReducible(left) => Mul(cs.reduce(left), right)
+      case Mul(left, right) if cs.isReducible(right) => Mul(left, cs.reduce(right))
       case Mul(Num(x), Num(y)) => Num(x * y)
     }
   }
--- src/main/scala/com/rr/kernel/Syntax.scala
+++ src/main/scala/com/rr/kernel/Syntax.scala
@@ -2,5 +2,5 @@ package com.rr.kernel
 
 sealed trait Syntax
 case class Num(value: Long) extends Syntax
-case class Add[A, B](left: A, right: B) extends Syntax
-case class Mul[A, B](left: A, right: B) extends Syntax
+case class Add(left: Syntax, right: Syntax) extends Syntax
+case class Mul(left: Syntax, right: Syntax) extends Syntax
